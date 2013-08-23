014e4b1a530d1bfbf9ce7e3b1d21809030a803e7
Nick Manley <nm3n8@umkc.edu>
2013-08-23 11:42:54 -0500

    Added Euler3.

--- src/euler/euler3.go
+++ src/euler/euler3.go
@@ -1,5 +1,16 @@
 package euler
 
 import (
-
+	"math"
 )
+
+func Euler3() int {
+	const num = 600851475143
+	var largestPrimeFactor int
+	for i := 2; i < int(math.Sqrt(num)); i++ {
+		if num % i == 0 && IsPrime(i) {
+			largestPrimeFactor = i
+		}
+	}
+	return largestPrimeFactor
+}
--- src/main.go
+++ src/main.go
@@ -6,5 +6,5 @@ import (
 )
 
 func main() {
-	fmt.Println( euler.PrimeSieve(100) )
+	fmt.Println( euler.Euler3() )
 }
